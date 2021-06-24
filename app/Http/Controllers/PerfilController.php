2691c9b511f2d8e4abd340a7186f581fa1e7982e
lucasgio <mikaelosoltes@gmail.com>
2021-06-23 23:24:26 -0400

    Final

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -64,13 +64,8 @@ class PerfilController extends Controller
         $perfil -> platos_preferidos = $request['platos_preferidos'];
         $perfil -> bio = $request['biografia'];
         if (request('imagen_perfil')) {
-<<<<<<< HEAD
             $storagePath = $request->file(key:'imagen_perfil')->store(path:'perfil',options:'spaces');
             $imgServer = Storage::disk(option:'spaces')->url($storagePath);
-=======
-            $storagePath = $request->file(key:'imagen_perfil')->store('recetas','spaces');
-            $imgServer = Storage::disk('spaces')->url($storagePath);
->>>>>>> dd022a8f18a70c967e598f9c2daf2f2c1cc0146a
             $perfil->imagen_perfil = $imgServer;
         }  
 
